b86d49d70932cade13727e9372e0f14f150d36f4
Curtis Lane <clanelmt@gmail.com>
2020-05-12 12:51:21 -0400

    update app.js, refactor, add comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,23 +4,16 @@ $(document).ready(function() {
     // Edamam API for recipe lookup
     const app_id = 'bfdf4f3e'
     const app_key = 'd3d66f2575260072a0894f4e9797d55a'
-    let q = 'chicken'
+    let q = 'chicken' // Will contain search input from idex.html
     
     let edamam_URL = 'https://api.edamam.com/search?q=' + q + '&app_id='+ app_id +'&app_key='+ app_key
     
+    // ajax query for Edamam API
     $.ajax({
         url: edamam_URL,
         method: 'GET'
     }).then(function(response){
         console.log(response)
     })
-    
-    
-    
-
-
-    
-    
-    
 
 }); // *Close document ready function

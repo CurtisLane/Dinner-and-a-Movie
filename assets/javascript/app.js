bdcef15e637985a7eac317934828d60ce5f800e3
Curtis Lane <clanelmt@gmail.com>
2020-05-12 12:48:17 -0400

    update app.js with first working api and ajax.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,34 +1,26 @@
 $(document).ready(function() {
+    console.log('Ready!')
 
-    // These will be set to equal whichever api url is chosen by the user in the first and second drop down menus
-    let dropdownLeft;
-    let dropdownRight;
-
-    let q = 'minecraft' // This will be set to the search input value on click
-    let limit;
+    // Edamam API for recipe lookup
+    const app_id = 'bfdf4f3e'
+    const app_key = 'd3d66f2575260072a0894f4e9797d55a'
+    let q = 'chicken'
     
-    const yt_api_key = 'AIzaSyDYbQo18NZbT_zT_wAfv3BjU1o1ziUWMRs'; // Api key from youtube
-    const redditPublicKey = 'NOoIuGKYf3u77g'
-    const redditSecretKey = 'Gr-4X_j_oawud4UJL8lyHyA8mPc'
+    let edamam_URL = 'https://api.edamam.com/search?q=' + q + '&app_id='+ app_id +'&app_key='+ app_key
     
-
-    // Search URL from YouTube API
-    let yt_url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=' + q + '&key=' + yt_api_key;
-    
-    // Reddit URL
-    let reddit_url = ''
-
-    // Wikipedia URL
-    let wiki_url = 'https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=' + q + '&format=json&origin=*'
-    
-    // Ajax function to test the query urls
-
     $.ajax({
-        url: wiki_url, // Currently testing Wikipedia URL
-        method: "GET"
-    }).then(function (response) {
+        url: edamam_URL,
+        method: 'GET'
+    }).then(function(response){
+        console.log(response)
+    })
+    
+    
+    
 
-        console.log(response);
-    }); // *Close ajax .then function
+
+    
+    
+    
 
 }); // *Close document ready function

1cb6f00b250e8fe40d5ad9797407c59de344286d
Curtis Lane <clanelmt@gmail.com>
2020-05-13 21:22:06 -0400

    update app.js, add click handler for search button. Update input values.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,41 @@
 $(document).ready(function() {
     console.log('Ready!')
 
-    // Edamam API for recipe lookup
-    const app_id = 'bfdf4f3e'
-    const app_key = 'd3d66f2575260072a0894f4e9797d55a'
-    let q = 'chicken thigh' // Will contain search input from idex.html
-    
-    let edamam_URL = 'https://api.edamam.com/search?q=' + q + '&app_id='+ app_id +'&app_key='+ app_key
-    
-    // ajax query for Edamam API
-    $.ajax({
-        url: edamam_URL,
-        method: 'GET'
-    }).then(function(response){
-        console.log(response)
+    // Define dayOrWeek based on user choice from dropdown
+    let dayOrWeek;
+    $('.dropdown-item').on('click', function(){
+        dayOrWeek = $(this).text()
     })
 
-    // The Movie DB(database) API for movie lookup by collection
-    let dayOrWeek = 'day' // Will be chosen by user via drop-down menu
-    const tmdb_api_key = 'b673088700e4fa8381c8b35441340dea'
-    let tmdb_URL = 'https://api.themoviedb.org/3/trending/movie/' + dayOrWeek + '?api_key=' + tmdb_api_key
-    
+    // Listen for click on search button
+    $('#searchButton').on('click', function(){
 
-    $.ajax({
-        url: tmdb_URL,
-        method: 'GET'
-    }).then(function(response){
-        console.log(response)
-    })
+        // Edamam API for recipe lookup
+        const app_id = 'bfdf4f3e'
+        const app_key = 'd3d66f2575260072a0894f4e9797d55a'
+        let q = $('#searchInput').val().trim() // Will contain search input from idex.html
+        let edamam_URL = 'https://api.edamam.com/search?q=' + q + '&app_id='+ app_id +'&app_key='+ app_key
+        
+        // ajax query for Edamam API
+        $.ajax({
+            url: edamam_URL,
+            method: 'GET'
+        }).then(function(response){
+            console.log(response)
+        })
+
+        // The Movie DB(database) API for movie lookup by collection
+        const tmdb_api_key = 'b673088700e4fa8381c8b35441340dea'
+        let tmdb_URL = 'https://api.themoviedb.org/3/trending/movie/' + dayOrWeek + '?api_key=' + tmdb_api_key
+
+        // ajax query for TmDB
+        $.ajax({
+            url: tmdb_URL,
+            method: 'GET'
+        }).then(function(response){
+            console.log(response)
+        })
+
+    }) // *Close search button click listener
 
 }); // *Close document ready function
